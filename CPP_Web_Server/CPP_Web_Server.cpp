8065672f3fb1b2c4507fe4fd881a95502917f52b
Grant <gnorrie12@gmail.com>
2022-09-15 23:20:25 +0200

    Router module compelte

--- CPP_Web_Server/CPP_Web_Server.cpp
+++ CPP_Web_Server/CPP_Web_Server.cpp
@@ -12,20 +12,24 @@
 #include "Cpp_Module_Types/Cpp_Module_Types/BaseModule.h"
 #include "Cpp_Module_Types/Cpp_Module_Types/WinUDPRxModule.h"
 #include "Cpp_Module_Types/Cpp_Module_Types/SessionProcModule.h"
+#include "Cpp_Module_Types/Cpp_Module_Types/RouterModule.h"
 
 int main()
 {
 	// Creating pipeline modules
-    auto pUDPRXModule = std::make_shared<WinUDPRxModule>("192.168.0.105", "8080", 2, 512);
-	auto pWAVSessionProcModule = std::make_shared<SessionProcModule>(2);
+    auto pUDPRXModule = std::make_shared<WinUDPRxModule>("192.168.0.105", "8080", 20, 512);
+	auto pWAVSessionProcModule = std::make_shared<SessionProcModule>(20);
+	auto pSessionChunkRouter = std::make_shared<RouterModule>(20);
 
 	// Connection pipeline modules
 	pUDPRXModule->SetNextModule(pWAVSessionProcModule);
-	pWAVSessionProcModule->SetNextModule(nullptr);
+	pWAVSessionProcModule->SetNextModule(pSessionChunkRouter);
+	pSessionChunkRouter->SetNextModule(nullptr);
 
 	// Starting pipeline modules
 	pUDPRXModule->StartProcessing();
 	pWAVSessionProcModule->StartProcessing();
+	pSessionChunkRouter->StartProcessing();
 
 	while (1)
 	{
